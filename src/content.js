4b99621ef665bb3027d7044d196f70b672329404
Prabuddha Raj Pandey <86178503+Prabuddhapandey@users.noreply.github.com>
2026-09-13 17:53:22 +0545

    Fix typo in apology message

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 /** Edit the only two visible lines here. */
 export const apology = {
   title: 'SORRY',
-  supportingLine: `Sorry Aakanshya !I will make sure this doesn't happen again real promise. :)`,
+  supportingLine: `Sorry Aakanshya !I will make sure this doesn't happen again I promise. :)`,
 }
